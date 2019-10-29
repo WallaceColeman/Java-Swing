b7681be94118b17ae236d9bc6e1ccb03ac0ac10b
WallaceColeman <wallardcoleman@gmail.com>
2019-10-29 11:13:38 -0400

    Added basic text area example needs more demonstations

--- Java Swing Example/src/JS/Java_Swing_Examples.java
+++ Java Swing Example/src/JS/Java_Swing_Examples.java
@@ -1,10 +1,30 @@
 package JS;
 
-public class Java_Swing_Examples {
+import javax.swing.*;
 
+public class Java_Swing_Examples {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		textExample();
 
 	}
-
+	
+	private static void textExample() {
+		JFrame frame = new JFrame("Text Example");
+		JTextArea text = new JTextArea("Text in a window");
+		
+		//             X   Y   W    H
+		text.setBounds(50, 50, 150, 20);
+		//X - Position on the X axis
+		//Y - Position on the Y axis
+		//W - Width
+		//H - Height
+		text.setEditable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(text);
+		frame.setSize(250,250);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+	
 }
